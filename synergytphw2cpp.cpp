42276aab859580fa0a951aa250277c0a5a5e7c51
Iofakh <it.khramov@gmail.com>
2022-11-02 14:48:35 +0300

    Добавьте файлы проекта.

---
+++ synergytphw2cpp.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+    int a, b;
+    printf("Please type 1st nunber: ");
+    scanf_s("%i", &a);
+    printf("Please type 1st nunber: ");
+    scanf_s("%i", &b);
+
+    if (a > b)
+    {
+        printf("The 1st number is greater then 2nd number");
+    }
+    else
+    {
+        if (a == b)
+        {
+            printf("The numbers are equal");
+        }
+        else
+        {
+            printf("The 1st number is less then 2nd number");
+        }
+    }
+}
